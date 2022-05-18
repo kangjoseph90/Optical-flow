e48a6ff7eaa23cdcf01f878469a930b5559a3112
kangjoseph90 <kangjoseph90@gmail.com>
2022-05-18 23:49:00 +0900

    ffff

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import pygame as pg
 import pygame.camera as camera
 from pygame.locals import *
@@ -11,12 +12,12 @@ import cv2
 pg.init()
 camera.init()
 
-size=[900,900]
+size=[1280,720]
 screen=pg.display.set_mode(size)
 
 clock=pg.time.Clock()
 
-cam=camera.Camera(camera.list_cameras()[0],(400,300))
+cam=camera.Camera(camera.list_cameras()[0],(320,180))
 cam.start()
 
 last_imgdata=pg.surfarray.array3d(cam.get_image()).swapaxes(0,1).astype(np.float32)
@@ -27,7 +28,7 @@ scale=2
     
 grid=20
 
-kernel1d=cv2.getGaussianKernel(2*grid-1,10)
+kernel1d=cv2.getGaussianKernel(2*grid-1,20)
 kernel=np.outer(kernel1d,kernel1d.transpose())
 
 def get_v(x,y,rt,rx,ry):
@@ -35,19 +36,18 @@ def get_v(x,y,rt,rx,ry):
     x=round(x)
     y=round(y)
 
-    nrt=rt[y-grid+1:y+grid,x-grid+1:x+grid]*kernel
-    nrx=rx[y-grid+1:y+grid,x-grid+1:x+grid]*kernel
-    nry=ry[y-grid+1:y+grid,x-grid+1:x+grid]*kernel
+    nrt=rt[y-grid+1:y+grid,x-grid+1:x+grid]
+    nrx=rx[y-grid+1:y+grid,x-grid+1:x+grid]
+    nry=ry[y-grid+1:y+grid,x-grid+1:x+grid]
 
-    a1=np.array([[np.sum(nrx*nrx),np.sum(nrx*nry)],[np.sum(nrx*nry),np.sum(nry*nry)]])
-    a2=np.array([[np.sum(nrx*nrt)],[np.sum(nry*nrt)]])
+    a1=np.array([[np.sum(nrx*nrx*kernel),np.sum(nrx*nry*kernel)],[np.sum(nrx*nry*kernel),np.sum(nry*nry*kernel)]])
+    a2=np.array([[np.sum(nrx*nrt*kernel)],[np.sum(nry*nrt*kernel)]])
 
     v=np.zeros((2,1))
 
-    if np.linalg.matrix_rank(a1)==a1.shape[0]:
-        v=-np.linalg.solve(a1,a2)
-
-    return v
+    if abs(np.linalg.det(a1)) <1: 
+        return np.zeros((2,1))
+    return -np.linalg.solve(a1,a2)
 
 
 while True:
@@ -60,24 +60,24 @@ while True:
 
     imgdata=pg.surfarray.array3d(frame).swapaxes(0,1).astype(np.float32)
     imgdata=cv2.cvtColor(imgdata,cv2.COLOR_BGR2GRAY)  #240,320
-    imgdata=cv2.bilateralFilter(imgdata,5,1,1)
+    imgdata=cv2.medianBlur(imgdata,3)
 
     rt=(imgdata-last_imgdata)/dt
-    rx=(np.roll(imgdata,-1,axis=1)-np.roll(imgdata,1,axis=1))/2
-    ry=(np.roll(imgdata,-1,axis=0)-np.roll(imgdata,1,axis=0))/2
-    
-    #plt.imshow(rt,cmap='gray')
-    #plt.show()
-    
+    rx=(np.roll(imgdata,-1,axis=1)-imgdata)
+    ry=(np.roll(imgdata,-1,axis=0)-imgdata)
 
     for y in range(grid,imgdata.shape[0]-grid+1,grid):
         for x in range(grid,imgdata.shape[1]-grid+1,grid):
 
             v=get_v(x,y,rt,rx,ry)
 
-            pg.draw.line(screen,(255,0,0),[scale*x,scale*y],[scale*x+v[0,0],scale*y+v[1,0]],2)
+            pg.draw.line(screen,(255,0,0),[scale*x,scale*y],[scale*x+v[0,0]/5,scale*y+v[1,0]/5],3)
 
-    #point+=get_v(point[0],point[1],rt,rx,ry)[:,0]*dt
+    temp=deepcopy(point)
+    point+=get_v(point[0],point[1],rt,rx,ry)[:,0]*dt*1.5
+    if point[0]-grid<0 or point[0]+grid>imgdata.shape[1] or point[1]-grid<0 or point[1]+grid>imgdata.shape[0]:
+        point=temp
+
 
     pg.draw.circle(screen,(0,0,255),(point*scale).astype(int),5)
 
